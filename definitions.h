5eac59d384eac682f9ac044a540a3b448c22cb56
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2014-07-04 11:49:47 +0200

    Better CGAL templating
    Signed-off-by: Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>

--- definitions.h
+++ definitions.h
@@ -22,23 +22,14 @@
 // Compile-time options
 // * if the code crashes, try to compile with EXACT_CONSTRUCTIONS so that
 //   robust arithmetic is used
-// * switch to the setdiff paradigm by defining SET_DIFF
 
 #define EXACT_CONSTRUCTIONS
+//#define COORDS_3D
 
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
 #include "TriangleInfo.h"
-
-// STL
-#include <iostream>
-#include <stack>
-#include <set>
-#include <list>
-#include <fstream>
-#include <math.h>
-#include <time.h>
 
 // OGR
 #include <gdal/ogrsf_frmts.h>
@@ -49,6 +40,9 @@
 #else
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #endif
+#ifdef COORDS_3D
+#include <CGAL/Projection_traits_xy_3.h>
+#endif
 #include <CGAL/Triangulation_face_base_with_info_2.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/Triangulation_hierarchy_2.h>
@@ -58,9 +52,16 @@
 
 // Kernel
 #ifdef EXACT_CONSTRUCTIONS
-typedef CGAL::Exact_predicates_exact_constructions_kernel K;
+typedef CGAL::Exact_predicates_exact_constructions_kernel TK;
+typedef CGAL::Exact_intersections_tag ET;
 #else
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel TK;
+typedef CGAL::Exact_predicates_tag ET;
+#endif
+#ifdef COORDS_3D
+typedef CGAL::Projection_traits_xy_3<TK> K;
+#else
+typedef TK K;
 #endif
 
 typedef CGAL::Triangulation_vertex_base_2<K> TVB;
@@ -68,12 +69,9 @@ typedef CGAL::Triangulation_hierarchy_vertex_base_2<TVB> VB;
 typedef CGAL::Constrained_triangulation_face_base_2<K> FB;
 typedef CGAL::Triangulation_face_base_with_info_2<TriangleInfo, K, FB> FBWI;
 typedef CGAL::Triangulation_data_structure_2<VB, FBWI> TDS;
-typedef CGAL::Exact_predicates_tag PT;
-typedef CGAL::Exact_intersections_tag IT;
-typedef CGAL::Constrained_Delaunay_triangulation_2<K, TDS, PT> CDT;
+typedef CGAL::Constrained_Delaunay_triangulation_2<K, TDS, ET> CDT;
 typedef CGAL::Triangulation_hierarchy_2<CDT> CDTH;
 typedef CGAL::Constrained_triangulation_plus_2<CDTH> Triangulation;
-typedef CGAL::Snap_rounding_traits_2<K> SRT;
 
 typedef Triangulation::Point Point;
 typedef K::Segment_2 Segment;
